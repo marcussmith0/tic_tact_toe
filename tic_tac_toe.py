9ccfee65bca51c3d976d618f9ba95dea9b9249a6
Marcus Smith <marcussmith0711@gmail.com>
2017-11-10 11:51:51 -0600

    re built version

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,164 +1,124 @@
-board = [
-    ["1.", "2.", "3."],
-    ["4.", "5.", "6."],
-    ["7.", "8.", "9."],
-]
+from IPython.display import clear_output
 
-player = ["player1", "player2"]
+board = ["1.", "2.", "3.", "4.", "5.", "6.", "7.", "8.", "9."]
 turn = 0
 
-is_playing = True    
-
-def print_board (board):
-    for row in board:
-        print " ".join(map(str, row))
-
-def change_board(option, value, board):
-    if option == "1":
-        board[0][0] = value
-        print_board(board)
-        check_winner(board)
-    elif option == "2":
-        board[0][1] = value
-        print_board(board)
-        check_winner(board)
-    elif option == "3":
-        board[0][2] = value
-        print_board(board)
-        check_winner(board)
-    elif option == "4":
-        board[1][0] = value
-        print_board(board)
-        check_winner(board)
-    elif option == "5":
-        board[1][1] = value
-        print_board(board)
-        check_winner(board)
-    elif option == "6":
-        board[1][2] = value
-        print_board(board)
-        check_winner(board)
-    elif option == "7":
-        board[2][0] = value
-        print_board(board)
-        check_winner(board)
-    elif option == "8":
-        board[2][1] = value
-        print_board(board)
-        check_winner(board)
-    elif option == "9":
-        board[2][2] = value
-        print_board(board)
-        check_winner(board)
-
-
-def check_winner(board):
-    global turn
-    if board[0][0] == board[0][1] == board[0][2] == "X":
-        print ""
-        print "Player 1 IS THE WINNER!" 
-        turn = 0
-        reset_board()
-    elif board[0][0] == board[0][1] == board[0][2] == "O":
-        print ""
-        print "Player 2 IS THE WINNER!" 
-        turn = 0
-        reset_board()
-    elif board[0][0] == board[1][0] == board [2][0] == "X":
-        print ""
-        print "Player 1 IS THE WINNER!"
-        turn = 0
-        reset_board()
-    elif board[0][0] == board[1][0] == board [2][0] == "O":
-        print ""
-        print "Player 2 IS THE WINNER!"
-        turn = 0
-        reset_board()
-    elif board[1][0] == board[1][1] == board[1][2] == "X":
-        print ""
-        print "Player 1 IS THE WINNER!"
-        turn = 0
-        reset_board()
-    elif board[1][0] == board[1][1] == board[1][2] == "O":
-        print ""
-        print "Player 2 IS THE WINNER!"
-        turn = 0
-        reset_board()
-    elif board[2][0] == board[2][1] == board[2][2] == "X":
-        print ""
-        print "Player 1 IS THE WINNER!"
-        turn = 0
-        reset_board()
-    elif board[2][0] == board[2][1] == board[2][2] == "O":
-        print ""
-        print "Player 2 IS THE WINNER!"
-        turn = 0
-        reset_board()
-    elif board[0][1] == board[1][1] == board[2][1] == "X":
-        print ""
-        print "Player 1 IS THE WINNER!"
-        turn = 0
-        reset_board()
-    elif board[0][1] == board[1][1] == board[2][1] == "O":
-        print ""
-        print "Player 2 IS THE WINNER!"
-        turn = 0
-        reset_board()
-    elif board[0][2] == board[1][2] == board[2][2] == "X":
-        print ""
-        print "Player 1 IS THE WINNER!"
-        turn = 0
-        reset_board()
-    elif board[0][2] == board[1][2] == board[2][2] == "O":
-        print ""
-        print "Player 2 IS THE WINNER!"
-        turn = 0
-        reset_board()
-    elif board[0][0] == board[1][1] == board[2][2] == "X":
-        print ""
-        print "Player 1 IS THE WINNER!"
-        turn = 0
-        reset_board()
-    elif board[0][0] == board[1][1] == board[2][2] == "O":
-        print ""
-        print "Player 2 IS THE WINNER!"
-        turn = 0
-        reset_board()
-    elif board[0][2] == board[1][1] == board[2][0] == "X":
-        print ""
-        print "Player 1 IS THE WINNER!"
-        turn = 0
-        reset_board()
-    elif board[0][2] == board[1][1] == board[2][0] == "O":
-        print ""
-        print "Player 2 IS THE WINNER!"
-        turn = 0
-        reset_board()
+def display_board(board):
+    print ""
+    print "   " + board[0] + " | " + board[1]  + " | " +  board[2]
+    print "   " + "____________"
+    print "   " + board[3] + " | " + board[4]  + " | " +  board[5]
+    print "   " + "____________"
+    print "   " + board[6] + " | " + board[7]  + " | " +  board[8]
+    print ""
     
+    player_input()
+
+def player_input():
+    global turn
+    position = raw_input("Place your marker: ")
+
+    if turn == 0:
+        turn = 1
+        place_marker("X", position)
+    elif turn == 1:
+        turn = 0
+        place_marker("O", position)
+
+def place_marker(value, place):
+    global board
+    if place == "1":
+        board[0] = value
+        check_winner()
+        display_board(board)
+    elif place == "2":
+        board[1] = value
+        check_winner()
+        display_board(board)
+    elif place == "3":
+        board[2] = value
+        check_winner()
+        display_board(board)
+    elif place == "4":
+        board[3] = value
+        check_winner()
+        display_board(board)
+    elif place == "5":
+        board[4] = value
+        check_winner()
+        display_board(board)
+    elif place == "6":
+        board[5] = value
+        check_winner()
+        display_board(board)
+    elif place == "7":
+        board[6] = value
+        check_winner()
+        display_board(board)
+    elif place == "8":
+        board[7] = value
+        check_winner()
+        display_board(board)
+    elif place == "9":
+        board[8] = value
+        check_winner()
+        display_board(board)
+    else:
+        print "Don't recognized that command."
 
 def reset_board():
     global board
-    board = [
-        ["1.", "2.", "3."],
-        ["4.", "5.", "6."],
-        ["7.", "8.", "9."],
-    ]
-    
-def process_input(player, input):
-    global turn
-    if player == "player1":
-        turn = 1
-        change_board(input, "X", board)
-    elif player == "player2":
-        turn = 0
-        change_board(input, "O", board)
+    board = ["1.", "2.", "3.", "4.", "5.", "6.", "7.", "8.", "9."]
+
+def check_winner():
+    if board[0] == board[1]  == board[2] == "X":
+        print "Player X wins"
+        reset_board()
+    elif board[0] == board[1]  == board[2] == "O":
+        print "Player O wins"
+        reset_board()
+    elif board[3] == board[4]  == board[5] == "X":
+        print "Player X wins"
+        reset_board()
+    elif board[3] == board[4]  == board[5] == "O":
+        print "Player O wins"
+        reset_board()
+    elif board[6] == board[7]  == board[8] == "X":
+        print "Player X wins"
+        reset_board()
+    elif board[6] == board[7]  == board[8] == "O":
+        print "Player O wins"
+        reset_board()
+    elif board[0] == board[3]  == board[6] == "X":
+        print "Player X wins"
+        reset_board()
+    elif board[0] == board[3]  == board[6] == "O":
+        print "Player O wins"
+        reset_board()
+    elif board[1] == board[4]  == board[7] == "X":
+        print "Player X wins"
+        reset_board()
+    elif board[1] == board[4]  == board[7] == "O":
+        print "Player O wins"
+        reset_board()
+    elif board[2] == board[5]  == board[8] == "X":
+        print "Player X wins"
+        reset_board()
+    elif board[2] == board[5]  == board[8] == "O":
+        print "Player O wins"
+        reset_board()
+    elif board[0] == board[4]  == board[8] == "X":
+        print "Player X wins"
+        reset_board()
+    elif board[0] == board[4]  == board[8] == "O":
+        print "Player O wins"
+        reset_board()
+    elif board[2] == board[4]  == board[6] == "X":
+        print "Player X wins"
+        reset_board()
+    elif board[2] == board[4]  == board[6] == "O":
+        print "Player O wins"
+        reset_board()
 
 
-def start():
-    global turn, player    
-    while is_playing:
-        print "Player Turn:", player[turn]
-        input = raw_input("Press a number to make a choice: ")
-        process_input(player[turn], input)
-
-start()
+display_board(board)
